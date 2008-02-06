0e16a7472307711f5df51d6d4f89f9bb38c5527b
Joe Orton <joe@manyfish.uk>
2008-02-06 20:54:26 +0000

    * autogen.sh: Fix bootstrap.

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@ libtoolize --force --copy
 aclocal
 autoheader
 autoconf
-automake --ignore-deps --foreign --copy --force
+automake --ignore-deps --foreign --copy --force --add-missing
 rm -rf autom4te*
